aac1a86fbd0b1b5c5a36d3bd708b508345da8108
gimddc <gimddc@whut.edu.cn>
2024-06-21 11:31:47 +0800

    feat:css && i18n

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
+import { useTranslation } from 'react-i18next';
+
 const StudyNotes = () => {
+  const { t } = useTranslation();
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '20%' }}>
-      <h1>软件设计开发与实践</h1>
+    <div style={{ textAlign: 'center', marginTop: '10%' }}>
+      <h1>{t('Home.Course')}</h1>
+      <h2>{t('Home.Name')}</h2>
+      <h2>0122104951104</h2>
     </div>
   );
 };
